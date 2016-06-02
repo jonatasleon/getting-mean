b7777e3ab61daa7af61a056ba3d5838776ac0027
Jonatas Leon <jonatas.leon@gmail.com>
2016-06-02 12:00:16 -0300

    Update controllers and views

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/* GET 'homelist' page */
+module.exports.homelist = function(req, res) {
+	res.render('index', { title: 'Home'});
+};
+
+/* GET 'locationInfo' page */
+module.exports.locationInfo = function(req, res) {
+	res.render('index', { title: 'Location Info'});
+};
+
+/* GET 'addReview' page */
+module.exports.addReview = function(req, res) {
+	res.render('index', { title: 'Add Review'});
+};

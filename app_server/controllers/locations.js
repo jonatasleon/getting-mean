5b2b478dc433517ffe378e613fa07a563573f7da
Jonatas Leon <jonatas.leon@gmail.com>
2016-06-04 16:26:31 -0300

    Update homepage

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,6 @@
 /* GET 'homelist' page */
 module.exports.homelist = function(req, res) {
-	res.render('index', { title: 'Home'});
+	res.render('locations-list', { title: 'Home'});
 };
 
 /* GET 'locationInfo' page */

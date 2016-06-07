f15dcfec17a570311198bd66f50fc3aeb2618c3f
Jonatas Leon <jonatas.leon@gmail.com>
2016-06-06 21:50:33 -0300

    Add the view templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,14 @@
 /* GET 'homelist' page */
 module.exports.homelist = function(req, res) {
-	res.render('locations-list', { title: 'Home'});
+	res.render('locations-list', { title: 'Home' });
 };
 
 /* GET 'locationInfo' page */
 module.exports.locationInfo = function(req, res) {
-	res.render('location-info', { title: 'Location Info'});
+	res.render('location-info', { title: 'Location Info' });
 };
 
 /* GET 'addReview' page */
 module.exports.addReview = function(req, res) {
-	res.render('index', { title: 'Add Review'});
+	res.render('location-review-form', { title: 'Add Review' });
 };

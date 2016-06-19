e5aeeab53d478031af047e179a1423780d83266f
Jonatas Leon <jonatas.leon@gmail.com>
2016-06-19 19:22:19 -0300

    Complete locations model

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,5 +1,33 @@
 var mongoose = require('mongoose');
 
+var openingTimeSchema = new mongoose.Schema({
+    days: {
+        type: String,
+        required: true
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true
+    }
+});
+
+var reviewSchema = new mongoose.Schema({
+    author: String,
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    reviewText: String,
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
+
 var locationSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -12,5 +40,13 @@ var locationSchema = new mongoose.Schema({
         min: 0,
         max: 5
     },
-    facilities: [String]
+    facilities: [String],
+    coords: {
+        type: [Number],
+        index: '2dsphere'
+    },
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
 });
+
+mongoose.model('Location', locationSchema);

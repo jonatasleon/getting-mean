cf003e65118957a01b3432956756878f5f34b669
Jonatas Leon <jonatas.leon@gmail.com>
2016-06-26 14:33:58 -0300

    Add app_api

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,48 @@
+var utils = require('./utils');
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+module.exports.locationsListByDistance = function(req, res) {
+    utils.sendJsonResponse(res, 200, {
+        "status": "success"
+    });
+};
+
+module.exports.locationsCreate = function(req, res) {
+    utils.sendJsonResponse(res, 200, {
+        "status": "sucess"
+    });
+};
+
+module.exports.locationsReadOne = function(req, res) {
+    if (req.params && req.params.locationid) {
+        Loc.findById(req.params.locationid).exec(function(err, location) {
+            if (!location){
+                utils.sendJsonResponse(res, 404, {
+                    "message": "locationid not found"
+                });
+                return;
+            }else if (err){
+                utils.sendJsonResponse(res, 404, err);
+                return;
+            }
+            utils.sendJsonResponse(res, 200, location);
+        });
+    } else {
+        utils.sendJsonResponse(res, 404, {
+            "message": "No locationid is request"
+        });
+    }
+};
+
+module.exports.locationsUpdateOne = function(req, res) {
+    utils.sendJsonResponse(res, 200, {
+        "status": "sucess"
+    });
+};
+
+module.exports.locationsDeleteOne = function(req, res) {
+    utils.sendJsonResponse(res, 200, {
+        "status": "sucess"
+    });
+};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,17 @@
+var utils = require('./utils');
+
+module.exports.reviewsCreate = function(req, res) {
+    utils.sendJsonResponse(res, 200, { "status": "success" });
+};
+
+module.exports.reviewsReadOne = function(req, res) {
+    utils.sendJsonResponse(res, 200, { "status": "success" });
+};
+
+module.exports.reviewsUpdateOne = function(req, res) {
+    utils.sendJsonResponse(res, 200, { "status": "success" });
+};
+
+module.exports.reviewsDeleteOne = function(req, res) {
+    utils.sendJsonResponse(res, 200, { "status": "success" });
+};

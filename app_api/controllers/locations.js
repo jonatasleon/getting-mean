57961985a3f976eb8d0129173ec4136cc7e11990
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-03 00:39:27 -0300

    Implements locationByDistance

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -4,10 +4,40 @@ var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
 module.exports.locationsListByDistance = function(req, res) {
-    Loc.find().exec(function(err, locations) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(20),
+        num: 10
+    };
+
+    if (!lat || !lng) {
+        utils.sendJsonResponse(res, 404, {
+            "message": "lat and lng query parameters are required"
+        });
+        return;
+    }
+
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations = [];
         if (err) {
             utils.sendJsonResponse(res, 404, err);
         } else {
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRads(doc.dis),
+                    name: doc.obj.name,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id
+                });
+            });
             utils.sendJsonResponse(res, 200, locations);
         }
     });

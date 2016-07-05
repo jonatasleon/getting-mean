07c679c5b9c25437e64a6c1cda6f5f9a50282857
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-04 22:46:08 -0300

    Implements locationCreate

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -44,8 +44,28 @@ module.exports.locationsListByDistance = function(req, res) {
 };
 
 module.exports.locationsCreate = function(req, res) {
-    utils.sendJsonResponse(res, 200, {
-        "status": "sucess"
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lat), parseFloat(req.parse.lng)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+        }]
+    }, function(err, location) {
+        if (err) {
+            utils.sendJsonResponse(res, 404, err);
+        } else {
+            utils.sendJsonResponse(res, 200, location);
+        }
     });
 };
 

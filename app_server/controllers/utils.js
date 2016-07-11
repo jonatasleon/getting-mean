43c58135ab37b99266bb764758f63bc5a5eba0df
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-10 22:26:05 -0300

    Fix distance display

--- app_server/controllers/utils.js
+++ app_server/controllers/utils.js
@@ -6,4 +6,17 @@ if (process.env.NODE_ENV === 'production') {
     apiOptions.server = "https://loc8r-infinite.herokuapp.com/";
 }
 
+function _formatDistance(distance) {
+    var numDistance, unit;
+    if (distance > 1) {
+        numDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+    } else {
+        numDistance = parseInt(distance * 1000, 10);
+        unit = 'm';
+    }
+    return numDistance + unit;
+}
+
 module.exports.apiOptions = apiOptions;
+module.exports._formatDistance = _formatDistance;

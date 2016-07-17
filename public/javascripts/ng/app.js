13f4183456843fb92a84b0d35884f2ab3e3dda92
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-17 20:09:35 -0300

    Add hard coded data to location list

--- public/javascripts/ng/app.js
+++ public/javascripts/ng/app.js
@@ -1,1 +1,23 @@
 angular.module('loc8rApp', []);
+
+var locationListController = ['$scope', function($scope) {
+    $scope.data = {
+        locations: [{
+            name: 'Burguer Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '0.296543',
+            _id: '5370a35f2536f6785f8dfb6a'
+        }, {
+            name: 'Costy',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+            distance: '0.7485142',
+            _id: '5370a35f2536f6785f8dfb6a'
+        }]
+    }
+}];
+
+angular.module('loc8rApp').controller('locationListController', locationListController);

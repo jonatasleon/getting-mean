06f4a8bb915084b9c989f596b3fe42e47edcec1d
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-23 23:36:06 -0300

    Add loc8rData service

--- public/javascripts/ng/app.js
+++ public/javascripts/ng/app.js
@@ -1,22 +1,8 @@
 angular.module('loc8rApp', []);
 
-var locationListController = ['$scope', function($scope) {
+var locationListController = ['$scope', 'loc8rData', function($scope, loc8rData) {
     $scope.data = {
-        locations: [{
-            name: 'Burguer Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '0.296543',
-            _id: '5370a35f2536f6785f8dfb6a'
-        }, {
-            name: 'Costy',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
-            distance: '0.7485142',
-            _id: '5370a35f2536f6785f8dfb6a'
-        }]
+        locations: loc8rData
     }
 }];
 
@@ -51,7 +37,26 @@ var ratingStars = function() {
     };
 };
 
+var loc8rData = function() {
+    return [{
+        name: 'Burguer Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '0.296543',
+        _id: '5370a35f2536f6785f8dfb6a'
+    }, {
+        name: 'Costy',
+        address: '250 Low Avenue, Writing',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+        distance: '0.7485142',
+        _id: '5370a35f2536f6785f8dfb6a'
+    }];
+}
+
 angular.module('loc8rApp')
     .controller('locationListController', locationListController)
     .filter('formatDistance', formatDistance)
-    .directive('ratingStars', ratingStars);
+    .directive('ratingStars', ratingStars)
+    .service('loc8rData', loc8rData);

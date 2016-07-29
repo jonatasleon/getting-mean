671f56582936960850edc0d3af0e0117e43152c5
Jonatas Leon <jonatas.leon@gmail.com>
2016-07-28 22:08:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
 var config = {
-    production: {
-        MONGOLAB_URI: process.env.MONGOLAB_URI,
-        SERVER: 'https://loc8r-infinite.herokuapp.com/'
-    },
-    dev: {
-        MONGOLAB_URI: 'mongodb://localhost/Loc8r',
-        SERVER: 'http://localhost:3000/'
-    }
+  production: {
+    MONGOLAB_URI: process.env.MONGOLAB_URI,
+    SERVER: 'https://loc8r-infinite.herokuapp.com/'
+  },
+  dev: {
+    MONGOLAB_URI: 'mongodb://localhost/Loc8r',
+    SERVER: 'http://localhost:3000/'
+  }
 };
 
 
 module.exports = function (env) {
-    return config[env || process.env.NODE_ENV] || config.dev;
+  return config[env || process.env.NODE_ENV] || config.dev;
 };
